772f4e62f686caf23c33323612d65d3916559db6
jasons <jasons@novaleaf.com>
2022-08-16 02:35:31 -0700

    two projects ch3

---
+++ 3CalcSum/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace _3CalcSum
+{
+    /// <summary>
+    /// add numbers from 1 to N
+    ///
+    /// INPUTS:  a number "N"
+    /// CALCULATION:  add 1 + 2+...+N
+    /// OUTPUT: the sum of all the numbers
+    /// </summary>
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                CalcSum();
+            }
+
+            //while (counter < input)
+            //{
+            //    sum += counter;
+
+            //}
+
+            //while (sum < input)
+            //{
+            //    sum += counter;
+            //}
+
+            //while (counter < input)
+            //{
+            //    var numb = 1;
+            //    counter +=( numb+1 );
+
+            //    sum = numb + numb + 1;
+            //}
+        }
+
+        private static void CalcSum()
+        {
+            var input = ReadHelper("Input a number, and we will add all numbers from 1 till that together and output the sum!");
+
+            //var sum = 0;
+            //var counter = input;
+
+            //while (counter >= 1)
+            //{
+            //    sum += counter;
+            //    counter -= 1;
+            //}
+
+
+            var sum = 0;
+            var counter = 1;
+
+            while (counter <= input)  //input = 3
+            {
+                //Console.WriteLine($"+{counter}");
+                sum += counter;
+                counter += 1;
+            }
+
+
+
+
+
+            Console.WriteLine($"Your answer is {sum}");
+        }
+
+        private static int ReadHelper(string message)
+        {
+            Console.WriteLine(message);
+            var input = Console.ReadLine();
+            var isValid = int.TryParse(input, out var intVal);
+
+            if (isValid)
+            {
+                Console.WriteLine($"{intVal}? okay!");
+                return intVal;
+            }
+            else
+            {
+                Console.WriteLine("Nova says, 'That is not correct, please try again.'  Enter a number!");
+                return ReadHelper(message);
+            }
+
+        }
+    }
+}
---
+++ DiceRoller/Program.cs
@@ -0,0 +1,32 @@
+﻿
+while (true)
+{
+    Console.WriteLine("================================================================= \n enter dice, ex: 2d4");
+    var input = Console.ReadLine(); // "2d4"
+
+    var split = input.Split("d"); // ["2","4"]
+
+    var diceCount = int.Parse(split[0]); // 2
+    var diceSides = int.Parse(split[1]); // 4
+
+    Console.WriteLine($"Rolling {diceCount} dice of {diceSides} sides...");
+    Console.WriteLine("rolla");
+    Console.WriteLine("rola");
+    Console.WriteLine("rollla");
+    Console.WriteLine("rollaa!");
+
+    var rand = new Random();
+
+    var throws = 0;
+    var diceTotals = 0;
+    while (throws < diceCount)
+    {
+        var dieResult = rand.Next(1, diceSides+1);
+        throws += 1;
+        diceTotals += dieResult;
+        Console.WriteLine($"You rolled {dieResult}");
+    }
+
+    Console.WriteLine($"Your total is {diceTotals}!!!!!");
+    Console.WriteLine("-loona  -*-");
+}
